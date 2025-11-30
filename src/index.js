69cd0aaebe574f04663625d06f31881ab4077009
vishnu <v@gmail.com>
2025-11-30 07:05:28 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 import './App.css';
-import { HashRouter } from "react-router-dom";   // ✅ ADD THIS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <HashRouter>         {/* ✅ WRAP YOUR APP */}
-      <App />
-    </HashRouter>
+    <App />
   </Provider>
 );
-
